be9892fb94be3a2b9506cdba2e79d4ba8d460170
Humerathestar1 <yasheajjjj@gmail.com>
2025-03-06 11:21:36 +0530

    Create fun.table.c

---
+++ fun.table.c
@@ -0,0 +1,16 @@
+# 6.fun.table
+#include <stdio.h>
+int table();
+int main(){
+    table();
+}
+
+int table()
+{
+    int i;
+    for(i=19;i<=190;i=i+19){
+        printf("%d\n",i);
+    }
+
+    return 0;
+}
