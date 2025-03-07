6dcd91197abcc6a90654bee1dbd0418b89ca755e
Humerathestar1 <yasheajjjj@gmail.com>
2025-03-07 10:34:05 +0530

    Create fun.addition.c

---
+++ fun.addition.c
@@ -0,0 +1,15 @@
+# 12.fun.addition
+#include <stdio.h>
+int fun();
+int main() {
+    fun();
+}
+int fun(){
+
+     for(int i=5;i<=50;i++){
+       if(i%2 !=0){
+          printf("%d\n",i);
+      }
+}
+return 0;
+}
